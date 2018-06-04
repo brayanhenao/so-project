7438384c540b6e37e8028a4aebc36794992656ba
Jefry Cardona <jeffrykr95@gmail.com>
2018-06-04 02:33:15 -0500

    Update containers.md

---
+++ A00056004_A00320232/opcional/containers.py
@@ -0,0 +1,17 @@
+from pylxd import Client
+client = Client()
+seleccion = input('Digite 1 para crear un contenedor o 2 para eliminarlo: ')
+
+if seleccion==1:
+        name = raw_input("Nombre para el contenedor: ")
+        imagen = raw_input("Imagen del contenedor ")
+        config = {'name': name, 'source': {'type': 'image', 'mode': 'pull', 'server': "https://cloud-images.ubuntu.com/daily", "protocol": "simplestreams", 'alias': 'bionic/amd64'}, 'profiles': ['profilename']}
+        container = client.containers.create(config, wait=True)
+        print('Contenedor creado exitosamente')
+elif seleccion==2:
+        id = raw_input("Nombre del contenedor que desea eliminar: ")
+        container = client.containers.get('my-container')
+        container.delete()
+        print("Contenedor borrado exitosamente")
+else:
+        print("Entrada invalida")
